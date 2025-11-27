2c74d48fa413eb486d52035b82661f01f6ccf904
gundapdhanaji <gundapdhanaji@gmail.com>
2025-11-27 13:35:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='w-full h-56 bg-blue-500 container mx-auto px-4'>
+          <h1>hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
